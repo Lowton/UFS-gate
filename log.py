14a4ee72b4caedbb46f8ff59cca116d77445918e
Lowton <iskeyiv@gmail.com>
2018-02-15 16:54:33 +0400

    set log

--- log.py
+++ log.py
@@ -1,13 +1,13 @@
 import logging # Протоколирование
 
 # Конфигурация протоколирования
-log = logging.getLogger('pomodoro')
+log = logging.getLogger('UFS')
 log.setLevel(logging.DEBUG)
 
 ch = logging.StreamHandler() # вывод на консоль
 ch.setLevel(logging.ERROR)
 
-fh = logging.FileHandler('pomo.log') # вывод в файл
+fh = logging.FileHandler('ufs.log') # вывод в файл
 fh.setLevel(logging.DEBUG)
 
 fformatter = logging.Formatter('%(asctime)s\t%(levelname)s: %(message)s')
@@ -19,8 +19,3 @@ fh.setFormatter(fformatter)
 log.addHandler(ch)
 log.addHandler(fh)
 
-log.debug('%s message', 'debug')
-log.info('info message')
-log.warn('warn message')
-log.error('error message')
-log.critical('critical message')
